d4b18b1dbd5a093f539455316e63b9e47e2674f8
Ben LaRoque <benjamin.laroque@gmail.com>
2017-02-06 22:45:59 -0800

    dripline demo file

--- html/dripline_rpc.php
+++ html/dripline_rpc.php
@@ -11,24 +11,31 @@ function local_update(name, value) {
 }
 </script>
 
+<!-- reusable list of sensor names -->
+<datalist id="sensors">
+  <option value="flux_capacitance"></option>
+  <option value="flux_inductance"></option>
+</datalist>
+
 <body>
 
   <h1>Simple dripline interface demo</h1>
 
   <h2>Retrieve value</h2>
   <form id="get_form">
-    Sensor name: <input type="text" name="get_target" list="get_targets">
-      <datalist id="get_targets">
-        <option value="flux_capacitance"></option>
-        <option value="flux_inductance"></option>
-      </datalist>
-    </select>
-    <button type="button" onclick=get_amqp(form.get_target.value,"amqp_here",local_update)>Get</button><br>
-    <pre id="amqp_here"></pre>
+    Sensor name: <input type="text" name="get_target" list="sensors" spellcheck=false>
+    <button type="button" onclick=get_amqp(form.get_target.value,"get_output",local_update)>Get</button><br>
+    <pre id="get_output"></pre>
   </form>
 
   <hr>
   <h2>Assign value</h2>
+  <form id="set_form">
+    Sensor name: <input type="text" name="set_target" list="sensors" spellcheck=false>
+    New value: <input type="text" name="set_value">
+    <button type="button" onclick=set_amqp(form.set_target.value,form.set_value.value,"set_output",local_update)>Set</button><br>
+    <pre id="set_output"></pre>
+  </form>
 
 </body>
 </html>
