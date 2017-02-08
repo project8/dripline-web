c75673c92167eba540381fe9c87cd09fd1aaf96c
Ben LaRoque <benjamin.laroque@gmail.com>
2017-02-07 22:53:47 -0800

    js cleanup

--- html/js/dripline_rpc_interface.js
+++ html/js/dripline_rpc_interface.js
@@ -21,7 +21,6 @@ function dripline_get(target, callback_args, callme)
     console.log("encoded:", JSON.stringify(msg));
     $.ajax({
         type: "POST",
-        //data: {"target":target, "msg":JSON.stringify(msg)},
         data: {"target":target, "msg":msg},
         datatype: "json",
         url: "php/dripline_amqp_client.php",
@@ -42,25 +41,4 @@ function dripline_get(target, callback_args, callme)
         }
     });
     console.log("ajax done");
-}
-
-function get_amqp(post_arg, callback_args, callme)
-{
-    console.log("in get_php (with ajax magix)");
-    console.log("post arg is", post_arg);
-    $.ajax({    
-        type: "POST",
-        data: {"value":post_arg},
-        //datatype: "json"
-        url: "php/demo_rpc_client.php",
-        success: function(result_data) {
-            console.log("rpc result data is: ", result_data);
-            callme(callback_args, result_data);
-        },
-        error: function(err1, err2, err3) {
-            console.log("e1: ", err1);
-            console.log("e2: ", err2);
-            console.log("e3: ", err3);
-        }
-    });
 }
